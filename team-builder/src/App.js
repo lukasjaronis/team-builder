da834846c6d9020eebd72ca2bcae02823ab55884
lukasjaronis <jaronisluke13@gmail.com>
2019-11-13 17:13:14 -0500

    completed

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css';
 import Form from "./Components/Form";
+import Cards from "./Components/Cards";
+
+
+
 
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  const addNewCard = card => {
+    const newCard = {
+      id: Date.now(),
+      firstname: card.firstname,
+      cohort: card.cohort,
+      favanimal: card.favanimal,
+      selection: card.selection
+    };
+    setCards([...cards, newCard]);
+  };
   return (
     <div className="App">
 
-    <Form />
+    <Form addNewCard={addNewCard} />
+    <Cards cards={cards} />
 
     </div>
   );
 }
 
 export default App;
+
+
--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,87 +1,146 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const Form = () => {
-    const [card, setCard] = useState({ title: ""}); 
+const FormStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 30rem;
+  width: 30rem;
 
-
-    const FormStyles = styled.div`
-    
+  form {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 30rem;
-    width: 30rem;
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
 
+    input {
+      padding: 5px;
+      margin: 5px;
+      border: 1px solid rgb(144, 238, 144);
 
-    form {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin: 0 auto;
-        width: 100%;
-        height: 100%;
-
-
-        input {
-            padding: 5px;
-            margin: 5px;
-            border: 1px solid rgb(144,238,144);
-
-            &::placeholder {
-                color: black;
-                letter-spacing: 1px;
-            }
-
-        }
-
-        button {
-            padding: 5px;
-            margin: 5px;
-        }
-
-        .selectionbox {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            margin: 5px;
-
-            label {
-                margin: 5px;
-            }
-
-            select {
-                width: 60%;
-            }
-
-        }
+      &::placeholder {
+        color: black;
+        letter-spacing: 1px;
+      }
     }
-    
-    `;
 
+    button {
+      padding: 5px;
+      margin: 5px;
+      background: #fff;
+      border: 2px solid black;
+      font-weight: 900;
+      letter-spacing: 1px;
+      line-height: 20px;
+      border-radius: 10px;
+      text-decoration: none;
+      text-transform: uppercase;
 
-return (
+      &:hover {
+        background: rgb(144, 238, 144);
+        border-radius: 12px;
+        border: 2px solid rgb(144, 238, 144);
+      }
+    }
+
+    .selectionbox {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin: 5px;
+
+      label {
+        margin: 5px;
+      }
+
+      select {
+        background: #fff;
+        border: 1.3px solid black;
+        width: 60%;
+        padding: 2px;
+
+        option {
+          color: black;
+        }
+      }
+    }
+  }
+`;
+
+const Form = props => {
+  const [card, setCard] = useState({
+    firstname: "",
+    cohort: "",
+    favanimal: "",
+    selection: ""
+  });
+
+  const handleChanges = e => {
+    setCard({ ...card, [e.target.name]: e.target.value });
+  };
+
+  const submitForm = e => {
+      e.preventDefault();
+      props.addNewCard(card);
+      setCard({ firstname: "", cohort: "", favanimal: "", selection: ""})
+  }
+
+  return (
     <FormStyles>
-    <form>
-    <input placeholder="First Name" id="firstname" type="text" name="firstname" />
-    <input placeholder="Cohort" id="cohort" type="text" name="cohort" />
-    <input placeholder="Favorite Animal" id="favanimal" type="text" name="favanimal" />
-    <div className="selectionbox">
-    <label>Which one of these is your current strongest?</label>
-    <select>
-    <option>HTML</option>
-    <option>HTML & Preprocessor</option>
-    <option>JavaScript</option>
-    <option>React</option>
-    </select>
-    </div>
-    <button type="submit">Submit</button>
-    </form>
+      <form onSubmit={submitForm}>
+        <input
+          value={card.firstname}
+          placeholder="First Name"
+          type="text"
+          name="firstname"
+          onChange={handleChanges}
+          required
+        />
+        <input
+          value={card.cohort}
+          placeholder="Cohort"
+          type="text"
+          name="cohort"
+          onChange={handleChanges}
+          required
+        />
+        <input
+          value={card.favanimal}
+          placeholder="Favorite Animal"
+          type="text"
+          name="favanimal"
+          onChange={handleChanges}
+          required
+        />
+        <div className="selectionbox">
+          <label htmlFor="selection">
+            Which one of these is your current strongest?
+          </label>
+          <select onChange={handleChanges} name="selection" id="selection">
+            <option name="option" id="option" type="text">
+              HTML
+            </option>
+            <option name="option" id="option" type="text">
+              HTML & Preprocessor
+            </option>
+            <option name="option" id="option" type="text">
+              JavaScript
+            </option>
+            <option name="option" id="option" type="text">
+              React
+            </option>
+          </select>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </FormStyles>
-);
+  );
 };
 
 export default Form;
